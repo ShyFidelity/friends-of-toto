d7eb79e100e89429a214276a0f165ea702a37892
jkworks <info@jilliankayworks.com>
2021-11-19 13:56:08 -0700

    new pet form working with drop down

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@
 import './styles/App.css';
 import Nav from './components/Navigation'
 import Login from './components/Login';
+import NewPet from './pages/NewPet'
 //import nav bar use in return 
 function App() {
   return (
     <div className="container">
     <Nav/>
     <Login/>
+    <NewPet />
     </div>
   );
 }
---
+++ client/src/pages/NewPet.js
@@ -0,0 +1,98 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import React, { useState } from 'react';
+
+const petTypes = [
+  {
+    value: 'Dog',
+    label: 'Dog',
+  },
+  {
+    value: 'Cat',
+    label: 'Cat',
+  },
+  {
+    value: 'Bird',
+    label: 'Bird',
+  },
+  {
+    value: 'Small Mammal',
+    label: 'Small Mammal',
+  },
+  {
+    value: 'Reptile',
+    label: 'Reptile',
+  },
+  {
+    value: 'Other',
+    label: 'Other',
+  },
+];
+
+
+
+
+
+export default function NewPet() {
+
+    const [petType, setPetType] = React.useState('EUR');
+
+  const handleChange = (event) => {
+    setPetType(event.target.value);
+  };
+
+
+    return (
+        <div className="container">
+            <h2>Hooray! Welcome to Friends of ToTo</h2>
+            <p>Tell us more about your friend</p>
+        <Box
+        className="inputElement"  
+        component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+  
+          <div>
+            <TextField
+              required
+              id="filled-required"
+              label="Required"
+              defaultValue="Pet Name"
+              variant="filled"
+            />
+            <TextField
+          id="filled-select-currency"
+          select
+          label="Select"
+          value={petType}
+          onChange={handleChange}
+          helperText="Pet Type"
+          variant="filled"
+        >
+          {petTypes.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          id="filled-multiline-static"
+          label="Bio"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+          variant="filled"
+        />
+       </div>
+           
+        </Box>
+        </div>
+      );
+
+}
+
