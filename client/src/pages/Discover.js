b60b3ee979ab1c75177e01b2ca846a46b9f411f5
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-11-21 12:29:29 -0700

    added placeholder text to Following and Discover pages to better show functionality

--- client/src/pages/Discover.js
+++ client/src/pages/Discover.js
@@ -4,6 +4,7 @@ export default function Discover() {
     return (
       <div>
         <Header />
+        <p>Posts from all account will appear here so you can discover new friends to follow!</p>
       </div>
     )
 }
