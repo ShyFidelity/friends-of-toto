e80e0ee00ce62ba178ce9d051f1d2147674ab369
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-11-20 18:06:27 -0700

    added nav links to header element for profile/following/discover

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,18 +4,21 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-      <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-        <Link className="text-dark" to="/following">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Following
-          </h1>
-        </Link>
-        <Link className="text-dark" to="/discover">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Discover
-          </h1>
-        </Link>
-      </div>
+      <Link className="text-dark" to="/profile">
+        <h1 className="m-0" style={{ fontSize: '3rem' }}>
+          Profile
+        </h1>
+      </Link>
+      <Link className="text-dark" to="/following">
+        <h1 className="m-0" style={{ fontSize: '3rem' }}>
+          Following
+        </h1>
+      </Link>
+      <Link className="text-dark" to="/discover">
+        <h1 className="m-0" style={{ fontSize: '3rem' }}>
+          Discover
+        </h1>
+      </Link>
     </header>
   );
 };
