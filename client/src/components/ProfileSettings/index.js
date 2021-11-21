edee6e567c67019dd5abd7c44e3949004e494a91
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-11-21 11:16:56 -0700

    made profile settings editable - still not connected to backend for user

--- client/src/components/ProfileSettings/index.js
+++ client/src/components/ProfileSettings/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -6,19 +6,36 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { TextField } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
   media: {
-    height: 140,
+    height: 345,
   },
 });
 
-export default function MediaCard() {
+// const profileSettings = {
+//   username: "Maverick",
+//   bio: "You are all living in my world. I'm chill, but do what I want"
+// }
+
+export default function ProfileSettings() {
   const classes = useStyles();
+
+  const [isEditable, setIsEditable] = useState(false);
+  const [buttonText, setButtonText] = useState("Edit");
+  const [username, setUsername] = useState("Maverick");
+  const [bio, setBio] = useState("You are all living in my world. I'm chill, but do what I want");
+
+  const handleEdit = () => {
+    setIsEditable(!isEditable);
+    if (buttonText === "Edit") {
+      setButtonText("Save")
+    } else {setButtonText("Edit")}
+  };
 
   return (
     <Card className={classes.root}>
@@ -28,19 +45,32 @@ export default function MediaCard() {
           image="/static/images/cards/contemplative-reptile.jpg"
           title="Contemplative Reptile"
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Bio: Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
       </CardActionArea>
+      <CardContent>
+        <TextField
+          required
+          id="filled-required"
+          label="Username"
+          value={username}
+          variant="filled"
+          disabled={!isEditable}
+          onChange={(e) => {setUsername(e.target.value)}}
+        />
+        <TextField
+          required
+          id="filled-required"
+          label="Bio"
+          multiline
+          rows={5}
+          value={bio}
+          variant="filled"
+          disabled={!isEditable}
+          onChange={(e) => setBio(e.target.value)}
+        />
+      </CardContent>
       <CardActions>
-        <Button size="small" color="primary">
-          Edit
+        <Button size="small" color="primary" onClick={handleEdit}>
+          {buttonText}
         </Button>
       </CardActions>
     </Card>
