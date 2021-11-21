aafe834d6436e01d2ff05112ea2295a1db77c00f
Nathan Irvin <65375604+irv0735@users.noreply.github.com>
2021-11-20 17:25:12 -0700

    additional placeholders for navigation - added signup and login forms along with auth/mutations

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,59 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import './styles/App.css';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Profile from './pages/Profile';
 import Following from './pages/Following';
 import Discover from './pages/Discover';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <div className="container">
-      {/* <Nav /> */}
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/following" component={Following} />
-        <Route exact path="/discover" component={Discover} />
-      </Switch>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' componentn={Signup} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/following" component={Following} />
+          <Route exact path="/discover" component={Discover} />
+        </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
