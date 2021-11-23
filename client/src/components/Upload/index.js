c8504ab327bddf3fdf37211badd7ffa679c34260
jkworks <info@jilliankayworks.com>
2021-11-23 14:20:36 -0700

    saved changes

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-
-import React, { useRef } from "react";
-import S3 from "react-aws-s3";
-
-function Upload() {
-  const fileInput = useRef();
-  const handleClick = (event) => {
-    event.preventDefault();
-    let file = fileInput.current.files[0];
-    let newFileName = fileInput.current.files[0].name.replace(/\..+$/, "");
-    const config = {
-      bucketName: process.env.REACT_APP_BUCKET_NAME,
-      region: process.env.REACT_APP_REGION,
-      accessKeyId: process.env.REACT_APP_ACCESS_ID,
-      secretAccessKey: process.env.REACT_APP_ACCESS_KEY,
-    };
-    const ReactS3Client = new S3(config);
-    ReactS3Client.uploadFile(file, newFileName).then((data) => {
-      console.log(data);
-      if (data.status === 204) {
-        console.log("success");
-      } else {
-        console.log("fail");
-      }
-    });
-  };
-  return (
-    <>
-      <form className='upload-steps' onSubmit={handleClick}>
-        <label>
-          Upload file:
-          <input type='file' ref={fileInput} />
-        </label>
-        <br />
-        <button type='submit'>Upload</button>
-      </form>
-    </>
-  );
-=======
 import React , {useState} from 'react';
 import { uploadFile } from 'react-s3';
 
@@ -72,7 +32,6 @@ const Upload = () => {
         <input type="file" onChange={handleFileInput}/>
         <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
     </div>
->>>>>>> b3279c6f15dadd20f6a591a3a8bb3a07788a6a83
 }
 
 export default Upload;
