9ad0fac5a1dfeae9a8cff30427815342ddcca395
Micheal Limones <micheal@zestylimones.com>
2021-11-30 18:47:43 -0700

    updated TypeDeff mutation for addPost.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -54,7 +54,7 @@ const typeDefs = gql`
     ): Auth
     login(email: String!, password: String!): Auth
     addFriend(username: String!): User
-    addPost(postText: String!): Post
+    addPost(postImage: String!, postText: String!): Post
     addComment(postId: ID!, commentText: String!): Post
     removePost(_id: ID!): Post
     removeComment(postId: ID!, commentId: ID!): Post
