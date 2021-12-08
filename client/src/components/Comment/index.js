fd4cb4695b668e93cdec2f225deb79be7211b18f
Micheal Limones <micheal@zestylimones.com>
2021-12-07 19:23:26 -0700

    added styling to comments.

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -1,37 +1,51 @@
-import * as React from "react";
+import * as React from 'react';
 
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
-import { Avatar } from "@mui/material";
-import { useQuery } from "@apollo/client";
-import { QUERY_USER } from "../../utils/queries";
-import { Typography } from "@mui/material";
+import { Avatar } from '@mui/material';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../../utils/queries';
+import { Typography } from '@mui/material';
 
 import './comment.css';
 
 export default function Comment(props) {
-  const {loading, data} = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(QUERY_USER, {
     variables: { username: props.commentAuthor },
   });
 
   return (
     <Box className="comment" style={{ width: '95%' }} sx={{ flexGrow: 1 }}>
-      <Grid container> 
-        <Grid item xs={12} sx={{ justifyContent: 'space-between'}}>
-          <Grid className="comment-header" container sx={{ alignItems: 'center' }}>
+      <Grid container>
+        <Grid item xs={12} sx={{ justifyContent: 'space-between' }}>
+          <Grid
+            className="comment-header"
+            container
+            sx={{ alignItems: 'center' }}
+          >
             <Grid item xs={6}>
-              { loading ? ( <div> loading... </div> ) : (
-                  <Avatar sx={{ marginLeft: '5px' }} alt="Profile Pic" src={ data.user.profilePic} />
+              {loading ? (
+                <div> loading... </div>
+              ) : (
+                <Avatar
+                  sx={{ marginLeft: '5px' }}
+                  alt="Profile Pic"
+                  src={data.user.profilePic}
+                />
               )}
             </Grid>
             <Grid item xs={6}>
-              <Typography sx={{ textAlign: 'right' }}>{props.commentAuthor}</Typography>
+              <Typography sx={{ textAlign: 'right' }}>
+                {props.commentAuthor}
+              </Typography>
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12}>
-          <Typography paragraph className="comment-text">{props.commentText}</Typography>
+          <Typography paragraph className="comment-text">
+            {props.commentText}
+          </Typography>
         </Grid>
       </Grid>
     </Box>
